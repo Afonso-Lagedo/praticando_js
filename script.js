ea74bdabd6b5566b413e6170de717bbc63f88842
unknown <afonso.ur@gmail.com>
2022-09-06 13:04:27 -0300

    array

--- script.js
+++ script.js
@@ -1,34 +1,6 @@
-var area = document.getElementById('area');
+var list = ['Afonso', 'Felipe', 'Rafaela', 420];//array or list
 
-function enter(){
-    var name = prompt("Enter name");
-
-    if(name === '' || name === null){
-        alert("OPS");
-        area.innerHTML = "Enter name"
-    }else{
-        area.innerHTML = "Hello" + name + " ";
-
-        let btnExit = document.createElement("button");//create button 
-        btnExit.innerText = "Exit";//text nutton
-        btnExit.onclick = exit;//call function exit
-        area.appendChild(btnExit);//add button in div area
-    }
-}
-
-function exit(){
-    alert("bye");
-    area.innerHTML = "BYE";
-}
-
-//####continue functions part2
-function avarage(n1, n2) { //use function in console!
-    var m=(n1+n2)/2;
-
-    if (m>=7){
-        console.log("Approved: " +m);
-    }else if(m<7){
-        console.log("Disapproved: " +m)
-    }
-
-}
+console.log(list);//all array
+console.log(list[0]);//position 1
+console.log(list[3]);//position 4
+console.log(list.length)// length of array

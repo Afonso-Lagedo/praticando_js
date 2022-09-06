b9195189561d7ac1850180b5fbae59541a1daee7
unknown <afonso.ur@gmail.com>
2022-09-06 12:22:59 -0300

    input

---
+++ script.js
@@ -0,0 +1,11 @@
+var name = prompt ("What is your name?");
+
+var lastName = prompt ("What is your last name?");
+
+console.log(name+ " " + lastName);
+
+document.write("<h1>Welcome "+name+" "+lastName+"</h1>");
+
+document.write("<img src='https://sujeitoprogramador.com/steve.png' alt='Foto'/>");
+
+ 

d6814a355e8f26a36a2633c21ef7659d1451db82
Afonso-cmd <afonso.ur@gmail.com>
2022-09-07 19:19:09 -0300

    timers

--- script.js
+++ script.js
@@ -1,30 +1,18 @@
-var mass;
-var height;
-var imc;
-var result;
+// setInterval: run function from time to time infinitely
 
-function calc(event){
-    event.preventDefault();//for no refresh
-
-    mass = document.getElementById('mass').value;
-    height = document.getElementById('height').value;
-
-    imc = mass / (height * height);
-
-    result = document.getElementById('result');
-
-    if(imc < 17){
-        result.innerHTML='<br/> Very low, IMC: '+imc.toFixed(2);
-    }else if(imc>17 && imc <=18.49){
-        result.innerHTML='<br/> Low, IMC: '+imc.toFixed(2);
-    }else if(imc>18.5 && imc<24.99){
-        result.innerHTML='<br/> Normal, IMC: '+imc.toFixed(2);
-    }else if(imc>25 && imc<29.99){
-        result.innerHTML='<br/> Above, IMC: '+imc.toFixed(2);
-    }else if(imc>=30){
-        result.innerHTML='<br/>Far above, IMC: '+imc.toFixed(2);
-    }
-
-    document.getElementById('mass').value='';
-    document.getElementById('height').value='';
+function action(){
+    document.write("Run SetInterval...</br>");
 }
+
+//setInterval(action, 2000);//1000=1s
+
+//with anonymous function
+/*var EXEMPLO =*/
+/*setInterval(()=>{
+    document.write("Run SetInterval Anonymous...</br>");
+}, 500);
+*/
+
+// for stoped: in console: clearInterval(EXEMPLO)
+
+setTimeout(action,3000);//run the function after the break
